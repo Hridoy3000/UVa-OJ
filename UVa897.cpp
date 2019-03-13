f1de2fd516f117e0f20af9fbd99ac9e593547205
Md Najmul Huda Hridoy <43324434+UVa3000@users.noreply.github.com>
2019-03-14 01:05:43 +0600

    Add files via upload

---
+++ UVa897.cpp
@@ -0,0 +1,38 @@
+//Md Najmul Huda Hridoy
+
+#include <cstdio>
+
+int main()
+{
+    
+    int anagramic[] = {2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, 97, 113, 131, 199, 311, 337, 373, 733, 919, 991};
+    int size = 22;
+    
+    
+    int wanted;
+    
+    while (scanf("%d", &wanted), wanted)
+    {
+        int best = size;
+        
+        for (int i = size - 1; i >= 0 && anagramic[i] > wanted; --i, --best);
+        
+        if (best < size)
+        {
+            int max = 1;
+            
+            while (wanted)
+            {
+                wanted /= 10;
+                max *= 10;
+            }
+            
+            if (anagramic[best] < max)
+                printf("%d\n", anagramic[best]);
+            else
+                printf("0\n");
+        }
+        else
+            printf("0\n");
+    }
+}
